11d145cfd76a7f4858dca7c349ea66ba4de00028
Anyachecheneva07@gmail.com <anyachecheneva07@gmail.com>
2022-10-05 23:29:47 +0200

    module value added

---
+++ module.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, te!");
+// Напишите программу вычисления модуля числа.
+// 	2 -> 2
+// 	-3 -> 3
+// 	-7 -> 7
+
+System.Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n < 0)
+{
+    n = -n ;
+    System.Console.WriteLine($" Модуль числа {-n} равен {n} ");
+}
+else 
+{
+    System.Console.WriteLine($" Модуль числа {n} равен {n} ");
+}
