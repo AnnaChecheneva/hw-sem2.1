6116d04f4e58f4ff226d1af2526df482228d18c7
Anyachecheneva07@gmail.com <anyachecheneva07@gmail.com>
2022-10-05 23:21:38 +0200

    inverse program added

---
+++ inverse.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Напишите программу, которая на вход принимает значение, а  на выходе показывает обратное значение.
+// 1 -> 1 
+// 2 -> 0.5
+// 0.25 -> 4
+
+Console.WriteLine("Hello, Vladimir!");
+System.Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+int o = 1;
+Console.WriteLine($" Обратным значением числа {n} является {(double) o / n}! ");
