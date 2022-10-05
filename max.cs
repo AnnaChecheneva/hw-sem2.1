84caa74a163a0a8d4afe030434eca98a631868c6
Anyachecheneva07@gmail.com <anyachecheneva07@gmail.com>
2022-10-05 23:27:00 +0200

    max value added

---
+++ max.cs
@@ -0,0 +1,14 @@
+﻿Console.WriteLine("Hello, Vladimir!");
+Console.WriteLine("Hello, World!");
+System.Console.WriteLine("Введите число a:");
+int a = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите число b:");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a>b)
+{
+    System.Console.WriteLine($"Число {a} больше числа {b}!");
+}
+else
+{
+    System.Console.WriteLine($"Число {b} больше числа {a}!");
+}
