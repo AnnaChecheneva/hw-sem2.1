9f1a69d10cd6d6f9c9947db3f0a4ee18ffe3c6a3
Anyachecheneva07@gmail.com <anyachecheneva07@gmail.com>
2022-10-05 23:00:55 +0200

    even added

---
+++ even.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+// Напишите программу, которая на вход принимает число и выдаёт, 
+// является ли число чётным (делится ли оно на два без остатка).
+System.Console.WriteLine("Введите число:");
+int n = Convert.ToInt32(Console.ReadLine());
+double i = n % 2;
+System.Console.WriteLine($"Остаток {i}!");
+if ( i == 0 )
+{
+    System.Console.WriteLine($"{n} четное число!");
+}
+else
+{
+    System.Console.WriteLine($"{n} нечетное число!");
+}
+
